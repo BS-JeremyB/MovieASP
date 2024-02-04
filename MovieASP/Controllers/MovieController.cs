b8ee5c5ee1eedb9e98e6cefa12c64b705590adf1
BS-JeremyB <jeremy.bazin@bstorm.be>
2024-02-04 11:32:24 +0100

    Ajoutez des fichiers projet.

---
+++ MovieASP/Controllers/MovieController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieASP.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
